cfc1e805059e70c32abc486c68d7562eaecf101a
Ryuivan <ryu.ivan@student.umn.ac.id>
2024-08-13 21:43:05 +0700

    fix: assets

--- src/components/data/CropTop.js
+++ src/components/data/CropTop.js
@@ -1,71 +1,77 @@
+import pinkCrop from "../../assets/product/crop/crop-pink-signature.jpg";
+import blackCrop from "../../assets/product/crop/crop-black.jpg";
+import greenCrop from "../../assets/product/crop/crop-green.jpg";
+import redCrop from "../../assets/product/crop/crop-red.jpg";
+import yellowCrop from "../../assets/product/crop/crop-yellow.jpg";
+
 const cropProducts = [
     {   
         id: 1,
         name: 'Pink Signature Crop Top',
-        image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117452492314259557/crop-pink-signature.webp",
+        image: pinkCrop,
         description: 'A stylish crop top made of 100% cotton.',
         price: '99.000',
         stock: 10,
         colors: [
             {
                 code: '#C39DBA',
-                image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117452492314259557/crop-pink-signature.webp"
+                image: pinkCrop
             }
         ]
     },
     {
         id: 2,
         name: 'Green Crop Top',
-        image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117465782637568072/crop-green.webp",
+        image: greenCrop,
         description: 'A stylish crop top made of 100% cotton.',
         price: '99.000',
         stock: 10,
         colors: [
             {
                 code: '#3EA590',
-                image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117465782637568072/crop-green.webp"
+                image: greenCrop
             },
         ]
     },
     {
         id: 3,
         name: 'Black Crop Top',
-        image: "https://i.pinimg.com/564x/93/ef/f8/93eff80dcc62f76c4ac044a5e4f40ff7.jpg",
+        image: blackCrop,
         description: 'A stylish crop top made of 100% cotton.',
         price: '99.000',
         stock: 10,
         colors: [
             {
                 code: '#626669',
-                image: "https://i.pinimg.com/564x/93/ef/f8/93eff80dcc62f76c4ac044a5e4f40ff7.jpg"
+                image: blackCrop
             },
         ]
     },
     {
         id: 4,
         name: 'Red Crop Top',
-        image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117469677535375491/crop-red.webp",
+        image: redCrop,
         description: 'A stylish crop top made of 100% cotton.',
         price: '99.000',
         stock: 10,
         colors: [
             {
                 code: '#BB4B63',
-                image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117469677535375491/crop-red.webp"
+                image: redCrop
             },
         ]
     },
     {
         id: 5,
         name: 'Yellow Crop Top',
-        image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117470995058540645/crop-yellow.webp",
+        image: yellowCrop,
         description: 'A stylish crop top made of 100% cotton.',
         price: '99.000',
         stock: 10,
         colors: [
             {
                 code: '#E4D793',
-                image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117470995058540645/crop-yellow.webp"
+                image: yellowCrop
             },
         ]
     }
--- src/components/data/ToteBag.js
+++ src/components/data/ToteBag.js
@@ -1,33 +1,35 @@
-const totebag = [
-    {
-        id: 23,
-        name: 'Pink Tote Bag',
-        image: "https://i.pinimg.com/236x/b9/f8/06/b9f806503ab73003c49196a9978c1dad.jpg",
-        description: 'A chic little green bag made entirely of canvas.',
-        price: '79.000',
-        stock: 10,
-        colors: [
-            {
-                code: '#C39DBA',
-                image: "https://i.pinimg.com/236x/b9/f8/06/b9f806503ab73003c49196a9978c1dad.jpg"
-            },
-        ]
+import pinkTote from "../../assets/product/tote bag/tote-pink.jpg";
+import yellowTote from "../../assets/product/tote bag/tote-yellow.jpg";
 
-    },
-    {
-        id: 24,
-        name: 'Yellow Tote Bag',
-        image: "https://i.pinimg.com/236x/d7/0a/b3/d70ab33eb333820e7fe48345721751fa.jpg",
-        description: 'A chic little yellow bag made entirely of canvas.',
-        price: '79.000',
-        stock: 10,
-        colors: [
-            {
-                code: '#fde97f',
-                image: "https://i.pinimg.com/236x/d7/0a/b3/d70ab33eb333820e7fe48345721751fa.jpg"
-            },
-        ]
-    }
-]
+const totebag = [
+  {
+    id: 23,
+    name: "Pink Tote Bag",
+    image: pinkTote,
+    description: "A chic little green bag made entirely of canvas.",
+    price: "79.000",
+    stock: 10,
+    colors: [
+      {
+        code: "#C39DBA",
+        image: pinkTote,
+      },
+    ],
+  },
+  {
+    id: 24,
+    name: "Yellow Tote Bag",
+    image: yellowTote,
+    description: "A chic little yellow bag made entirely of canvas.",
+    price: "79.000",
+    stock: 10,
+    colors: [
+      {
+        code: "#fde97f",
+        image: yellowTote,
+      },
+    ],
+  },
+];
 
 export default totebag;
