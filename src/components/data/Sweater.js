36049c60d64816da4a6d2509ef2209f244a09698
Vnesiav <viancavanesia@gmail.com>
2024-08-13 21:34:16 +0700

    feat: assets

--- src/components/data/Sweater.js
+++ src/components/data/Sweater.js
@@ -1,85 +1,92 @@
+import yellowSignatureSweater from '../../assets/product/sweater/sweater-yellow-signature.jpg';
+import pinkSignatureSweater from '../../assets/product/sweater/sweater-pink-signature.jpg';
+import blackSweater from '../../assets/product/sweater/sweater-black.jpg';
+import greenSweater from '../../assets/product/sweater/sweater-green.jpg';
+import redSweater from '../../assets/product/sweater/sweater-red.jpg';
+import yellowSweater from '../../assets/product/sweater/sweater-yellow.jpg';
+
 const sweater = [
     {
         id: 13,
         name: 'Yellow Signature Sweater',
-        image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117455565254303794/sweater-yellow-signature.webp",
+        image: yellowSignatureSweater,
         description: 'A sweater that offers high levels of comfort and warmth.',
         price: '199.000',
         stock: 10,
         colors: [
             {
                 code: '#fde97f',
-                image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117455565254303794/sweater-yellow-signature.webp"
+                image: yellowSignatureSweater
             },
         ]
     },
     {
         id: 12,
         name: 'Pink Signature Sweater',
-        image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117455566537760889/sweater-pink-signature.webp",
+        image: pinkSignatureSweater,
         description: 'A sweater that offers high levels of comfort and warmth.',
         price: '199.000',
         stock: 10,
         colors: [
             {
                 code: '#C39DBA',
-                image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117455566537760889/sweater-pink-signature.webp"
+                image: pinkSignatureSweater
             },
         ]
     },
     {
         id: 15,
         name: 'Black Sweater',
-        image: "https://i.pinimg.com/564x/3a/7e/cc/3a7ecc921cdabd22ba162db510417e26.jpg",
+        image: blackSweater,
         description: 'A sweater that offers high levels of comfort and warmth.',
         price: '199.000',
         stock: 10,
         colors: [
             {
                 code: '#626669',
-                image: "https://i.pinimg.com/564x/3a/7e/cc/3a7ecc921cdabd22ba162db510417e26.jpg"
+                image: blackSweater
             },
         ]
     },
     {
         id: 14,
         name: 'Green Sweater',
-        image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117465782281048114/sweater-green.webp",
+        image: greenSweater,
         description: 'A sweater that offers high levels of comfort and warmth.',
         price: '199.000',
         stock: 10,
         colors: [
             {
                 code: '#3EA590',
-                image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117465782281048114/sweater-green.webp"
+                image: greenSweater
             },
         ]
     },
     {
         id: 16,
         name: 'Red Sweater',
-        image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117469676407103589/sweater-red.webp",
+        image: redSweater,
         description: 'A sweater that offers high levels of comfort and warmth.',
         price: '199.000',
         stock: 10,
         colors: [
             {
                 code: '#BB4B63',
-                image: "https://cdn.discordapp.com/attachments/1091562630591811594/1117469676407103589/sweater-red.webp"
+                image: redSweater
             },
         ]
     },
     {
         id: 17,
         name: 'Yellow Sweater',
-        image: "https://i.pinimg.com/564x/16/b2/71/16b2719600d728daadf85f0de8095256.jpg",
+        image: yellowSweater,
         description: 'A sweater that offers high levels of comfort and warmth.',
         price: '199.000',
         stock: 10,
         colors: [
             {
                 code: '#E4D793',
-                image: "https://i.pinimg.com/564x/04/fc/66/04fc66c4893b425b676b9ec1d2fb4e79.jpg"
+                image: yellowSweater
             },
         ]
     }
